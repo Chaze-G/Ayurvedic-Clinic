333a6678c700585b04cf4095ebf76b9484d657cd
chamo <chamo@CHAZE>
2026-06-22 19:40:30 +0530

    Login page fully completed

--- Ayurvedic Clinic/Frontend/Forms/Login.cs
+++ Ayurvedic Clinic/Frontend/Forms/Login.cs
@@ -16,5 +16,61 @@ namespace Ayurvedic_Clinic.Frontend.Forms
         {
             InitializeComponent();
         }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnEye_Click(object sender, EventArgs e)
+        {
+            // switch between dotting the password and showing characters
+            txtPassword.UseSystemPasswordChar = !txtPassword.UseSystemPasswordChar;
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
