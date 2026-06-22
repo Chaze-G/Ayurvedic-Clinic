8840cb87ae61ae59e6abe237f9ce0a023c87945e
chamo <chamo@CHAZE>
2026-06-22 22:25:15 +0530

    Registration form done

---
+++ Ayurvedic Clinic/Frontend/Forms/AdminDashboard.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Ayurvedic_Clinic.Frontend.Forms
+{
+    partial class AdminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDashboard));
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPageTitle = new System.Windows.Forms.Label();
+            this.panelPatientCard = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRegisterPatient = new System.Windows.Forms.Button();
+            this.panelDoctorCard = new System.Windows.Forms.Panel();
+            this.btnRegisterDoctor = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panelPharmacistCard = new System.Windows.Forms.Panel();
+            this.btnRegisterPharmacist = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panelHeader.SuspendLayout();
+            this.panelPatientCard.SuspendLayout();
+            this.panelDoctorCard.SuspendLayout();
+            this.panelPharmacistCard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(232)))), ((int)(((byte)(239)))));
+            this.panelHeader.Controls.Add(this.label1);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(1001, 45);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Suwa Sewana - Admin";
+            // 
+            // lblPageTitle
+            // 
+            this.lblPageTitle.AutoSize = true;
+            this.lblPageTitle.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPageTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.lblPageTitle.Location = new System.Drawing.Point(369, 107);
+            this.lblPageTitle.Name = "lblPageTitle";
+            this.lblPageTitle.Size = new System.Drawing.Size(234, 50);
+            this.lblPageTitle.TabIndex = 1;
+            this.lblPageTitle.Text = "Registration";
+            // 
+            // panelPatientCard
+            // 
+            this.panelPatientCard.BackColor = System.Drawing.Color.White;
+            this.panelPatientCard.Controls.Add(this.pictureBox1);
+            this.panelPatientCard.Controls.Add(this.btnRegisterPatient);
+            this.panelPatientCard.Controls.Add(this.label3);
+            this.panelPatientCard.Controls.Add(this.label2);
+            this.panelPatientCard.Location = new System.Drawing.Point(48, 327);
+            this.panelPatientCard.Name = "panelPatientCard";
+            this.panelPatientCard.Size = new System.Drawing.Size(280, 320);
+            this.panelPatientCard.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label2.Location = new System.Drawing.Point(20, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Patient";
+            // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(130)))), ((int)(((byte)(140)))));
+            this.label3.Location = new System.Drawing.Point(22, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 80);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Add new patients, fill intake forms, and document initial holistic health profile" +
+    "s.";
+            // 
+            // btnRegisterPatient
+            // 
+            this.btnRegisterPatient.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(216)))), ((int)(((byte)(204)))));
+            this.btnRegisterPatient.FlatAppearance.BorderSize = 0;
+            this.btnRegisterPatient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegisterPatient.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegisterPatient.ForeColor = System.Drawing.Color.White;
+            this.btnRegisterPatient.Location = new System.Drawing.Point(20, 245);
+            this.btnRegisterPatient.Name = "btnRegisterPatient";
+            this.btnRegisterPatient.Size = new System.Drawing.Size(240, 45);
+            this.btnRegisterPatient.TabIndex = 2;
+            this.btnRegisterPatient.Text = "Register Patient";
+            this.btnRegisterPatient.UseVisualStyleBackColor = false;
+            this.btnRegisterPatient.Click += new System.EventHandler(this.btnRegisterPatient_Click);
+            // 
+            // panelDoctorCard
+            // 
+            this.panelDoctorCard.BackColor = System.Drawing.Color.White;
+            this.panelDoctorCard.Controls.Add(this.pictureBox2);
+            this.panelDoctorCard.Controls.Add(this.btnRegisterDoctor);
+            this.panelDoctorCard.Controls.Add(this.label4);
+            this.panelDoctorCard.Controls.Add(this.label5);
+            this.panelDoctorCard.Location = new System.Drawing.Point(358, 327);
+            this.panelDoctorCard.Name = "panelDoctorCard";
+            this.panelDoctorCard.Size = new System.Drawing.Size(280, 320);
+            this.panelDoctorCard.TabIndex = 3;
+            // 
+            // btnRegisterDoctor
+            // 
+            this.btnRegisterDoctor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(216)))), ((int)(((byte)(204)))));
+            this.btnRegisterDoctor.FlatAppearance.BorderSize = 0;
+            this.btnRegisterDoctor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegisterDoctor.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegisterDoctor.ForeColor = System.Drawing.Color.White;
+            this.btnRegisterDoctor.Location = new System.Drawing.Point(20, 245);
+            this.btnRegisterDoctor.Name = "btnRegisterDoctor";
+            this.btnRegisterDoctor.Size = new System.Drawing.Size(240, 45);
+            this.btnRegisterDoctor.TabIndex = 2;
+            this.btnRegisterDoctor.Text = "Register Patient";
+            this.btnRegisterDoctor.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(130)))), ((int)(((byte)(140)))));
+            this.label4.Location = new System.Drawing.Point(22, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 80);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Onboard Ayurvedic practitioners, assign Ayurvedic medical specialties, and config" +
+    "ure clinic schedules.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label5.Location = new System.Drawing.Point(20, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Doctor";
+            // 
+            // panelPharmacistCard
+            // 
+            this.panelPharmacistCard.BackColor = System.Drawing.Color.White;
+            this.panelPharmacistCard.Controls.Add(this.pictureBox3);
+            this.panelPharmacistCard.Controls.Add(this.btnRegisterPharmacist);
+            this.panelPharmacistCard.Controls.Add(this.label6);
+            this.panelPharmacistCard.Controls.Add(this.label7);
+            this.panelPharmacistCard.Location = new System.Drawing.Point(668, 327);
+            this.panelPharmacistCard.Name = "panelPharmacistCard";
+            this.panelPharmacistCard.Size = new System.Drawing.Size(280, 320);
+            this.panelPharmacistCard.TabIndex = 4;
+            // 
+            // btnRegisterPharmacist
+            // 
+            this.btnRegisterPharmacist.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(216)))), ((int)(((byte)(204)))));
+            this.btnRegisterPharmacist.FlatAppearance.BorderSize = 0;
+            this.btnRegisterPharmacist.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegisterPharmacist.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegisterPharmacist.ForeColor = System.Drawing.Color.White;
+            this.btnRegisterPharmacist.Location = new System.Drawing.Point(20, 245);
+            this.btnRegisterPharmacist.Name = "btnRegisterPharmacist";
+            this.btnRegisterPharmacist.Size = new System.Drawing.Size(240, 45);
+            this.btnRegisterPharmacist.TabIndex = 2;
+            this.btnRegisterPharmacist.Text = "Register Pharmacist";
+            this.btnRegisterPharmacist.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(130)))), ((int)(((byte)(140)))));
+            this.label6.Location = new System.Drawing.Point(22, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(240, 80);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Create credentials for dispensary staff to manage herbal inventory and distribute" +
+    " medicines.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label7.Location = new System.Drawing.Point(20, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 25);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Pharmacist";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label8.Location = new System.Drawing.Point(207, 221);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(574, 21);
+            this.label8.TabIndex = 5;
+            this.label8.Text = " Select a Department to register and onboard new members into the clinic system";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(25, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(20, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(25, 21);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(235, 95);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // AdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(247)))), ((int)(((byte)(246)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1001, 799);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panelPharmacistCard);
+            this.Controls.Add(this.panelDoctorCard);
+            this.Controls.Add(this.panelPatientCard);
+            this.Controls.Add(this.lblPageTitle);
+            this.Controls.Add(this.panelHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AdminDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AyurClinic Hub - Admin";
+            this.Load += new System.EventHandler(this.AdminDashboard_Load);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelPatientCard.ResumeLayout(false);
+            this.panelPatientCard.PerformLayout();
+            this.panelDoctorCard.ResumeLayout(false);
+            this.panelDoctorCard.PerformLayout();
+            this.panelPharmacistCard.ResumeLayout(false);
+            this.panelPharmacistCard.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPageTitle;
+        private System.Windows.Forms.Panel panelPatientCard;
+        private System.Windows.Forms.Button btnRegisterPatient;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panelDoctorCard;
+        private System.Windows.Forms.Button btnRegisterDoctor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panelPharmacistCard;
+        private System.Windows.Forms.Button btnRegisterPharmacist;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ Ayurvedic Clinic/Frontend/Forms/AdminDashboard.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ayurvedic_Clinic.Frontend.Forms
+{
+    public partial class AdminDashboard : Form
+    {
+        public AdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegisterPatient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdminDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace Ayurvedic_Clinic.Frontend.Forms
+//{
+//    public partial class AdminDashboard : Form
+//    {
+//        public AdminDashboard()
+//        {
+//            InitializeComponent();
+//        }
+
+//        // 1. Patient Registration Button Click
+//        private void btnRegisterPatient_Click(object sender, EventArgs e)
+//        {
+//            // This opens the Patient Registration form as a separate window
+//            PatientRegistrationForm patientForm = new PatientRegistrationForm();
+//            patientForm.ShowDialog();
+//        }
+
+//        // 2. Doctor Registration Button Click
+//        private void btnRegisterDoctor_Click(object sender, EventArgs e)
+//        {
+//            DoctorRegistrationForm doctorForm = new DoctorRegistrationForm();
+//            doctorForm.ShowDialog();
+//        }
+
+//        // 3. Pharmacist Registration Button Click
+//        private void btnRegisterPharmacist_Click(object sender, EventArgs e)
+//        {
+//            PharmacistRegistrationForm pharmacistForm = new PharmacistRegistrationForm();
+//            pharmacistForm.ShowDialog();
+//        }
+//    }
+//}
