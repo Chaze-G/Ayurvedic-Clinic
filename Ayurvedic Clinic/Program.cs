2f4870f32ae199f3bd38713143e037ffe0029092
Sithmi Manthrirathna <binasham3@gmail.com>
2026-06-25 13:47:35 +0530

    labels to buttons

--- Ayurvedic Clinic/Program.cs
+++ Ayurvedic Clinic/Program.cs
@@ -17,17 +17,17 @@ namespace Ayurvedic_Clinic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Pharmacist_registration());
+          //  Application.Run(new Pharmacist_registration());
             Application.Run(new doctor_prescription());
-            Application.Run(new Patient_registration());
-            Application.Run(new MedicalPharmacy());
+           // Application.Run(new Patient_registration());
+            /*Application.Run(new MedicalPharmacy());
             Application.Run(new PatientsHistory());
 
             Application.Run(new DoctorRegistor());
             Application.Run(new ChildRegistration());
 
             Application.Run(new Login());
-            Application.Run(new AdminDashboard());
+            Application.Run(new AdminDashboard());*/
 
         }
     }
