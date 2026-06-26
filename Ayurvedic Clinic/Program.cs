7b3be46eaf8906c7c5fd75a4510c10e2d67323e5
Sithmi Manthrirathna <binasham3@gmail.com>
2026-06-26 11:57:50 +0530

    finally done with uf updates added them to dr.prescriptiond dpnotetext

--- Ayurvedic Clinic/Program.cs
+++ Ayurvedic Clinic/Program.cs
@@ -7,13 +7,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System;
+using System.Windows.Forms;
+
 namespace Ayurvedic_Clinic
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
