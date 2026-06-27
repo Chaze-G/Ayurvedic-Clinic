83e7795c3990a0032ec3f90641a92e50f45daf91
Kaveesa <227543385+kavee40404@users.noreply.github.com>
2026-06-27 23:09:01 +0530

    Errors are fixed

--- Ayurvedic Clinic/Frontend/Forms/Login.Designer.cs
+++ Ayurvedic Clinic/Frontend/Forms/Login.Designer.cs
@@ -66,10 +66,9 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(237, 267);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(178, 217);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(437, 480);
+            this.panel1.Size = new System.Drawing.Size(328, 390);
             this.panel1.TabIndex = 1;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -78,10 +77,9 @@
             this.registerlink.AutoSize = true;
             this.registerlink.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.registerlink.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(201)))), ((int)(((byte)(193)))));
-            this.registerlink.Location = new System.Drawing.Point(81, 406);
-            this.registerlink.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.registerlink.Location = new System.Drawing.Point(61, 330);
             this.registerlink.Name = "registerlink";
-            this.registerlink.Size = new System.Drawing.Size(262, 23);
+            this.registerlink.Size = new System.Drawing.Size(200, 17);
             this.registerlink.TabIndex = 8;
             this.registerlink.TabStop = true;
             this.registerlink.Text = "Register through the receptionist";
@@ -91,10 +89,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(112, 372);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(84, 302);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(193, 23);
+            this.label5.Size = new System.Drawing.Size(145, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "Don\'t Have an account?";
             // 
@@ -105,10 +102,9 @@
             this.loginbut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loginbut.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loginbut.ForeColor = System.Drawing.Color.White;
-            this.loginbut.Location = new System.Drawing.Point(40, 295);
-            this.loginbut.Margin = new System.Windows.Forms.Padding(4);
+            this.loginbut.Location = new System.Drawing.Point(30, 240);
             this.loginbut.Name = "loginbut";
-            this.loginbut.Size = new System.Drawing.Size(368, 39);
+            this.loginbut.Size = new System.Drawing.Size(276, 32);
             this.loginbut.TabIndex = 6;
             this.loginbut.Text = "Login";
             this.loginbut.UseVisualStyleBackColor = false;
@@ -118,10 +114,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.DimGray;
-            this.label4.Location = new System.Drawing.Point(40, 192);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(30, 156);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 23);
+            this.label4.Size = new System.Drawing.Size(68, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Password ";
             // 
@@ -130,10 +125,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.DimGray;
-            this.label3.Location = new System.Drawing.Point(40, 123);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(30, 100);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 23);
+            this.label3.Size = new System.Drawing.Size(71, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "Username ";
             // 
@@ -144,19 +138,17 @@
             this.panel3.Controls.Add(this.pictureBox4);
             this.panel3.Controls.Add(this.loginpasswordtxt);
             this.panel3.Controls.Add(this.pictureBox3);
-            this.panel3.Location = new System.Drawing.Point(40, 217);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Location = new System.Drawing.Point(30, 176);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(367, 33);
+            this.panel3.Size = new System.Drawing.Size(276, 27);
             this.panel3.TabIndex = 3;
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(327, 2);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Location = new System.Drawing.Point(245, -1);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(40, 28);
+            this.pictureBox4.Size = new System.Drawing.Size(30, 23);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 2;
             this.pictureBox4.TabStop = false;
@@ -166,10 +158,9 @@
             // 
             this.loginpasswordtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.loginpasswordtxt.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginpasswordtxt.Location = new System.Drawing.Point(40, 5);
-            this.loginpasswordtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.loginpasswordtxt.Location = new System.Drawing.Point(30, 4);
             this.loginpasswordtxt.Name = "loginpasswordtxt";
-            this.loginpasswordtxt.Size = new System.Drawing.Size(261, 22);
+            this.loginpasswordtxt.Size = new System.Drawing.Size(196, 18);
             this.loginpasswordtxt.TabIndex = 1;
             this.loginpasswordtxt.UseSystemPasswordChar = true;
             this.loginpasswordtxt.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
@@ -179,9 +170,8 @@
             this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(247)))), ((int)(((byte)(245)))));
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
             this.pictureBox3.Location = new System.Drawing.Point(-1, -1);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(33, 33);
+            this.pictureBox3.Size = new System.Drawing.Size(25, 27);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 0;
             this.pictureBox3.TabStop = false;
@@ -192,21 +182,19 @@
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.loginusernametxt);
             this.panel2.Controls.Add(this.pictureBox2);
-            this.panel2.Location = new System.Drawing.Point(40, 146);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Location = new System.Drawing.Point(30, 119);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(367, 33);
+            this.panel2.Size = new System.Drawing.Size(276, 27);
             this.panel2.TabIndex = 2;
             // 
             // loginusernametxt
             // 
             this.loginusernametxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.loginusernametxt.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginusernametxt.Location = new System.Drawing.Point(40, 4);
-            this.loginusernametxt.Margin = new System.Windows.Forms.Padding(4);
+            this.loginusernametxt.Location = new System.Drawing.Point(30, 3);
             this.loginusernametxt.Multiline = true;
             this.loginusernametxt.Name = "loginusernametxt";
-            this.loginusernametxt.Size = new System.Drawing.Size(261, 27);
+            this.loginusernametxt.Size = new System.Drawing.Size(196, 22);
             this.loginusernametxt.TabIndex = 1;
             this.loginusernametxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -215,9 +203,8 @@
             this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(247)))), ((int)(((byte)(245)))));
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(-1, -1);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(33, 34);
+            this.pictureBox2.Size = new System.Drawing.Size(25, 28);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
@@ -227,10 +214,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(128)))), ((int)(((byte)(150)))));
-            this.label2.Location = new System.Drawing.Point(80, 70);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(60, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(281, 23);
+            this.label2.Size = new System.Drawing.Size(211, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Sign in to continue to your account";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -241,10 +227,9 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(85)))), ((int)(((byte)(104)))));
-            this.label1.Location = new System.Drawing.Point(88, 16);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(66, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(242, 35);
+            this.label1.Size = new System.Drawing.Size(194, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome Back";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -252,35 +237,34 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(174)))), ((int)(((byte)(192)))));
-            this.panel4.Location = new System.Drawing.Point(237, 684);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Location = new System.Drawing.Point(178, 556);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(107, 1);
+            this.panel4.Size = new System.Drawing.Size(80, 1);
             this.panel4.TabIndex = 8;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.textBox2.Location = new System.Drawing.Point(281, 25);
+            this.textBox2.Location = new System.Drawing.Point(211, 20);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(164, 22);
+            this.textBox2.Size = new System.Drawing.Size(124, 20);
             this.textBox2.TabIndex = 9;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Ayurvedic_Clinic.Properties.Resources.background_img_3;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(893, 1009);
+            this.ClientSize = new System.Drawing.Size(670, 820);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel4);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
--- Ayurvedic Clinic/Frontend/Forms/SearchID.Designer.cs
+++ Ayurvedic Clinic/Frontend/Forms/SearchID.Designer.cs
@@ -48,10 +48,9 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(173, 273);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(130, 222);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(368, 345);
+            this.panel1.Size = new System.Drawing.Size(276, 280);
             this.panel1.TabIndex = 0;
             // 
             // btnSearch
@@ -61,10 +60,9 @@
             this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSearch.ForeColor = System.Drawing.Color.White;
-            this.btnSearch.Location = new System.Drawing.Point(77, 252);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Location = new System.Drawing.Point(58, 205);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(187, 44);
+            this.btnSearch.Size = new System.Drawing.Size(140, 36);
             this.btnSearch.TabIndex = 5;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = false;
@@ -73,43 +71,37 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Ayurvedic_Clinic.Properties.Resources.background_img_3;
-            this.pictureBox1.Location = new System.Drawing.Point(20, 171);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
-            //this.pictureBox1.Image = global::Ayurvedic_Clinic.Properties.Resources.id_icon;
             this.pictureBox1.Location = new System.Drawing.Point(15, 139);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(47, 37);
+            this.pictureBox1.Size = new System.Drawing.Size(35, 30);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // txtPatientID
             // 
-            this.txtPatientID.Location = new System.Drawing.Point(63, 171);
-            this.txtPatientID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPatientID.Location = new System.Drawing.Point(47, 139);
             this.txtPatientID.Multiline = true;
             this.txtPatientID.Name = "txtPatientID";
-            this.txtPatientID.Size = new System.Drawing.Size(243, 36);
+            this.txtPatientID.Size = new System.Drawing.Size(183, 30);
             this.txtPatientID.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(16, 140);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 114);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 19);
+            this.label3.Size = new System.Drawing.Size(63, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Patient ID :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 82);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(307, 16);
+            this.label2.Size = new System.Drawing.Size(249, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter the Patient ID Number to view medical history";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -118,23 +110,21 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 26);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(23, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(263, 32);
+            this.label1.Size = new System.Drawing.Size(207, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Search Patient Record";
             // 
             // SearchID
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Ayurvedic_Clinic.Properties.Resources.background_img_3;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(696, 910);
+            this.ClientSize = new System.Drawing.Size(522, 739);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SearchID";
             this.Text = "SearchID";
             this.panel1.ResumeLayout(false);
