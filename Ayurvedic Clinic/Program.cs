dcf4174a5101e5864f0c8f928748bec149d7824d
Sithmi Manthrirathna <binasham3@gmail.com>
2026-06-27 21:39:51 +0530

    final linkings all done

--- Ayurvedic Clinic/Program.cs
+++ Ayurvedic Clinic/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using Ayurvedic_Clinic.Frontend.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace Ayurvedic_Clinic
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+
+            /*Application.Run(new Login());                    
+            Application.Run(new AdminDashboard());
+            Application.Run(new DoctorRegistor());
+            Application.Run(new Patient_registration());
+            Application.Run(new PatientsHistory());
+            Application.Run(new MedicalPharmacy());
+            Application.Run(new ChildRegistration());
+            Application.Run(new Pharmacist_registration());
+            Application.Run(new SearchID());*/
+
+
+            Application.Run(new doctor_prescription());
+
+        }
+    }
+}
