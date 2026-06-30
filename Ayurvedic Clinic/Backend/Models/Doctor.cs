19d550dd3915cebe42b9dac7e1401b680f73b182
Madusha Sewmina <224913277+madushasewmina456-pixel@users.noreply.github.com>
2026-06-30 08:57:45 +0530

    adding inheritance and abstraction

---
+++ Ayurvedic Clinic/Backend/Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayurvedic_Clinic.Backend.Models
+{
+   public class Doctor : person
+    {
+        public string AMCNumber { get; set; }
+        public string Specilization { get; set; }
+    }
+}
