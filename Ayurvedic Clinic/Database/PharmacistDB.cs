c61d154088fedee5b2f578c305d532c0214a2bd1
Kaveesa <227543385+kavee40404@users.noreply.github.com>
2026-07-05 03:03:13 +0530

    PharmacistDB.cs created

---
+++ Ayurvedic Clinic/Database/PharmacistDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayurvedic_Clinic.Database
+{
+    internal class PharmacistDB
+    {
+    }
+}
