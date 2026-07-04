3ff2298d509bd234998f7b25f8696192fda59eef
Kaveesa <227543385+kavee40404@users.noreply.github.com>
2026-07-04 17:25:18 +0530

    Comments on program.cs,patient ,Connection.cs updated. phamrcay services removed

--- Ayurvedic Clinic/Database/DBConnection.cs
+++ Ayurvedic Clinic/Database/DBConnection.cs
@@ -4,9 +4,8 @@ namespace Ayurvedic_Clinic.Database
 {
     public static class DBConnection
     {
-        // Your personal connection string
         private static string connectionString =
-            "Server=(localdb)\\MSSQLLocalDB\\SQLEXPRESS;Database=SuwasewanaDB;Trusted_Connection=True;";
+            "Server=(localdb)\\MSSQLLocalDB;Database=SuwasewanaDB;Trusted_Connection=True;";
 
         public static SqlConnection GetConnection()
         {
