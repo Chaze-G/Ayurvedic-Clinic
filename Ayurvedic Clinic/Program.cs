1efe6d99d42b2a235e52142a93edb3ccdf1ad4ae
Sithmi Manthrirathna <binasham3@gmail.com>
2026-07-04 11:21:03 +0530

    dr.prescription finally running with all linked pages  but need to fix more the history

--- Ayurvedic Clinic/Program.cs
+++ Ayurvedic Clinic/Program.cs
@@ -25,10 +25,10 @@ namespace Ayurvedic_Clinic
             // Application.Run(new DoctorRegistor());
             Application.Run(new Patient_registration());
 
-           /*sithmi's trail hard coded sample
+          /* sithmi's trail hard coded sample
             doctor_prescription docForm = new doctor_prescription("AMC-TEST-001"); 
             
-            docForm.LoadPatient("200562001632");
+            docForm.LoadPatient("200562001632c1");
 
             Application.Run(docForm);*/
 
