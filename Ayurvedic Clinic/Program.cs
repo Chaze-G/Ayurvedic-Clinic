7c5e5cbb69234eeb9aedc75223bce6d76aee59f0
Kaveesa <227543385+kavee40404@users.noreply.github.com>
2026-07-06 16:01:37 +0530

    Patient Registration Gender error Fixed

--- Ayurvedic Clinic/Program.cs
+++ Ayurvedic Clinic/Program.cs
@@ -18,7 +18,7 @@ namespace Ayurvedic_Clinic
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Login ());
+            Application.Run(new Patient_registration ());
             // Application.Run(new doctor_prescription());
             //Application.Run(new Login ());
             //Application.Run(new AdminDashboard());
